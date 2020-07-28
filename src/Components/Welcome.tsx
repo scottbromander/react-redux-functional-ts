75833c2335590dbf316bdd6a2b0f85a7c9391ceb
Scott Bromander <scottbromander@gmail.com>
2020-07-28 13:17:04 -0500

    propTypes and defaultProps added

--- src/Components/Welcome.tsx
+++ src/Components/Welcome.tsx
@@ -1,24 +1,49 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, MouseEvent, useState } from 'react';
+import PropTypes, { InferProps } from 'prop-types';
 
-type WelcomeProps = {
-  name: string;
-  position: string;
-  location?: string;
-};
+// type WelcomeProps = {
+//   name: string;
+//   position: string;
+//   location?: string;
+// };
 
-export const Welcome: FunctionComponent<WelcomeProps> = ({
+export function Welcome({
   name,
   position,
   location,
-}: WelcomeProps) => (
-  <div>
-    <h2>{name}</h2>
-    {location ? (
-      <p>
-        {position} - {location}
-      </p>
-    ) : (
-      <p>{position}</p>
-    )}
-  </div>
-);
+  children,
+}: InferProps<typeof Welcome.propTypes>) {
+  const [upvote, setUpvote] = useState(0);
+
+  const onButtonClick = (event: MouseEvent) => {
+    setUpvote(upvote + 1);
+  };
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      {location ? (
+        <p>
+          {position} - {location}
+        </p>
+      ) : (
+        <p>{position}</p>
+      )}
+      <button onClick={onButtonClick}>Upvote: {upvote}</button>
+    </div>
+  );
+}
+
+Welcome.propTypes = {
+  name: PropTypes.string.isRequired,
+  position: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+Welcome.defaultProps = {
+  location: 'Online',
+};
